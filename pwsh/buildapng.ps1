c51a381ee48ccbb9379df0b7ec72c5f59ba14b29
J.D. Purcell <jdpurcell@gmail.com>
2024-05-04 16:43:28 -0400

    QtApng: Build with Qt 6.5.3

--- pwsh/buildapng.ps1
+++ pwsh/buildapng.ps1
@@ -3,31 +3,68 @@
 $qtVersion = ((qmake --version -split '\n')[1] -split ' ')[3]
 Write-Host "Detected Qt Version $qtVersion"
 
+$useQmake = $qtVersion -like '5.*' -or $qtVersion -like '6.[0-4].*'
+$universalBinary = $IsMacOS -and $qtVersion -notlike '5.*'
+
 # Clone
 git clone https://github.com/jdpurcell/QtApng.git
 cd QtApng
-git checkout 4a74aac43eb0ba4787f9e8ef8cf0fb1ef6b14792
+if ($useQmake) {
+    git checkout qmake
+} else {
+    git checkout cmake
+}
 
-# Build
-
-# vcvars on windows
+# Dependencies
 if ($IsWindows) {
     & "$env:GITHUB_WORKSPACE/pwsh/vcvars.ps1"
 }
-
-$qmakeArgs = @(
-    "CONFIG+=libpng_static"
-)
-if ($IsMacOS -and $qtVersion -notlike '5.*') {
-    $qmakeArgs += "QMAKE_APPLE_DEVICE_ARCHS=""x86_64 arm64"""
+if (-not $useQmake) {
+    if ($IsWindows) {
+        choco install ninja pkgconfiglite
+    } elseif ($IsMacOS) {
+        brew update
+        brew install ninja
+    } else {
+        sudo apt-get install ninja-build
+    }
 }
-Write-Host "Running 'qmake', args: $qmakeArgs"
-Invoke-Expression "qmake $qmakeArgs"
 
-if ($IsWindows) {
-    Write-Host "Running 'nmake'"
-    nmake
+# Build
+if ($useQmake) {
+    mkdir build
+    cd build
+
+    $argDeviceArchs = $universalBinary ? "QMAKE_APPLE_DEVICE_ARCHS=x86_64 arm64" : $null;
+    qmake .. CONFIG+=libpng_static $argDeviceArchs
+
+    if ($IsWindows) {
+        nmake
+    } else {
+        make
+    }
+
+    cd ..
 } else {
-    Write-Host "Running 'make'"
-    make
+    cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
+    ninja -C build
+
+    if ($universalBinary) {
+        cmake -B build_arm64 -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=arm64
+        ninja -C build_arm64
+    }
+}
+
+# Copy output
+$outputDir = "output"
+mkdir $outputDir
+$files = Get-ChildItem -Path "build/plugins/imageformats" | Where-Object { $_.Extension -in ".dylib", ".dll", ".so" }
+foreach ($file in $files) {
+    if ($universalBinary -and -not $useQmake) {
+        $name = $file.Name
+        lipo -create "$file" "build_arm64/plugins/imageformats/$name" -output "$outputDir/$name"
+        lipo -info "$outputDir/$name"
+    } else {
+        Copy-Item -Path $file -Destination $outputDir
+    }
 }
