6008a1810130cd96fc225c9b77142cbeb466270a
J.D. Purcell <jdpurcell@gmail.com>
2024-11-04 00:51:06 -0500

    Test

--- pwsh/get-vcpkg-deps.ps1
+++ pwsh/get-vcpkg-deps.ps1
@@ -120,15 +120,15 @@ function WriteManifest() {
     }
 
     AddDependency 'zlib'
-    #AddDependency 'libjxl'
-    #AddDependency 'openexr'
-    #AddDependency 'libraw'
+    AddDependency 'libjxl'
+    AddDependency 'openexr'
+    AddDependency 'libraw'
     AddDependency 'libavif' @('dav1d')
     if ($IsLinux) {
         # x265 is only needed for encoding so we normally skip it, but this breaks the Linux build
-        #AddDependency 'libheif'
+        AddDependency 'libheif'
     } else {
-        #AddDependency 'libheif' $null true
+        AddDependency 'libheif' $null true
     }
 
     if (-not $IsWindows) {
@@ -137,7 +137,7 @@ function WriteManifest() {
     }
     if ($kde_vers -like 'v5.*' -or $kde_vers -like 'v6.[0-7].*') {
         # OpenEXR 3.3 introduced a change that's only compatible with KDE Frameworks 6.8+
-        #AddOverride 'openexr' '3.2.4'
+        AddOverride 'openexr' '3.2.4'
     }
 
     $manifest | ConvertTo-Json -Depth 5 | Out-File -FilePath "$vcpkgManifestDir/vcpkg.json"
@@ -146,7 +146,7 @@ function WriteManifest() {
 function InstallPackages() {
     WriteOverlayTriplet
 
-    WriteOverlayPorts
+    #WriteOverlayPorts
 
     WriteManifest
 
@@ -157,6 +157,8 @@ function InstallPackages() {
 }
 
 InstallPackages
+
+Copy-Item -Path "$env:GITHUB_WORKSPACE/util/libavif-static-fix/libavif-config.cmake" -Destination "$env:VCPKG_ROOT/installed-$env:VCPKG_DEFAULT_TRIPLET/$env:VCPKG_DEFAULT_TRIPLET/share/libavif"
 
 # Build x64-osx dependencies separately--we'll have to combine stuff later.
 if ($IsMacOS -and $env:buildArch -eq 'Universal') {
