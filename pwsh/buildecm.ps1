e79eaf55b640ae45d699df7d31d74977df7a7518
J.D. Purcell <jdpurcell@gmail.com>
2024-11-28 21:53:53 -0500

    Update vcpkgGitCommitId / KDE Frameworks

--- pwsh/buildecm.ps1
+++ pwsh/buildecm.ps1
@@ -1,11 +1,16 @@
 #!/usr/bin/env pwsh
 
+$qtVersion = [version]((qmake --version -split '\n')[1] -split ' ')[3]
+
+$kde_vers = $args[0]
+
 # Clone
 git clone https://invent.kde.org/frameworks/extra-cmake-modules.git
 cd extra-cmake-modules
-git checkout $args[0]
+git checkout $kde_vers
 
 # Build
+$argQt6 = $qtVersion.Major -eq 6 ? '-DBUILD_WITH_QT6=ON' : $null
 if ($IsMacOS) {
     $argDeviceArchs =
         $env:buildArch -eq 'X64' ? '-DCMAKE_OSX_ARCHITECTURES=x86_64' :
@@ -13,13 +18,13 @@ if ($IsMacOS) {
         $env:buildArch -eq 'Universal' ? '-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64' :
         $null
 }
-cmake -G Ninja . $argDeviceArchs
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX="$PWD/installed" -DCMAKE_BUILD_TYPE=Release $argQt6 $argDeviceArchs .
 
 if ($IsWindows) {
     ninja install
-    $env:ECM_DIR = "$PWD\installed\Program Files (x86)\ECM\share\ECM"
 } else {
     sudo ninja install
 }
+$env:ECM_DIR = "$PWD/installed/share/ECM"
 
 cd ../
