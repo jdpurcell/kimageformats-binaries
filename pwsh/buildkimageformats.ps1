jdpurcell/kimageformats-binaries

commit 5035c509cf86c95c20517e2e6abe731718ae1899

Update KDE Frameworks to 6.26, bump vcpkg commit

pwsh/buildkimageformats.ps1
@@ -4,7 +4,7 @@ $qtVersion = [version](qmake -query QT_VERSION)
 Write-Host "Detected Qt version $qtVersion"
 
 $kfGitRef =
-    $qtVersion -ge [version]'6.9.0' ? 'v6.25.0' :
+    $qtVersion -ge [version]'6.9.0' ? 'v6.26.0' :
     $qtVersion -ge [version]'6.8.0' ? 'v6.24.0' :
     $qtVersion -ge [version]'6.7.0' ? 'v6.17.0' :
     $(throw 'Unsupported Qt version.')

.github/workflows/KImageFormats.yml
@@ -3,7 +3,7 @@ on: [workflow_dispatch, pull_request]
 
 env:
   VCPKG_BINARY_SOURCES: 'clear;default,readwrite'
-  VCPKG_COMMIT_ID: 'bb4f525cbddd409bdc714ee3043b15ff7766af59'
+  VCPKG_COMMIT_ID: '99a97de2cb371449d4fb9dc970f2ac562d689ec2'
 
 jobs:
   build:

commit 0d33e9015e963554da70b83b44167036260a6a8c

Fix JXR/HEIF
Regression in 5035c509cf86c95c20517e2e6abe731718ae1899

pwsh/buildkimageformats.ps1
@@ -8,6 +8,9 @@ $kfGitRef =
     $qtVersion -ge [version]'6.8.0' ? 'v6.24.0' :
     $qtVersion -ge [version]'6.7.0' ? 'v6.17.0' :
     $(throw 'Unsupported Qt version.')
+$kfTagVersion =
+    $kfGitRef -match '^v(\d+\.\d+\.\d+)$' ? [version]$Matches[1] :
+    $null
 $kfMajorVer = 6
 $kimgLibExt =
     $IsWindows ? '.dll' :
@@ -25,7 +28,10 @@ patch CMakeLists.txt "../util/kimageformats$kfMajorVer-find-libraw-vcpkg.patch"
 rm "cmake/find-modules/FindLibRaw.cmake"
 
 # Patch CMakeLists to work with vcpkg's jxrlib, bypassing KImageFormats' FindLibJXR module.
-patch CMakeLists.txt "../util/kimageformats$kfMajorVer-find-jxrlib-vcpkg.patch"
+$patchNameSuffix =
+    $kfTagVersion -and $kfTagVersion -lt [version]'6.26.0' ? '' :
+    '-b'
+patch CMakeLists.txt "../util/kimageformats$kfMajorVer-find-jxrlib-vcpkg$patchNameSuffix.patch"
 rm "cmake/find-modules/FindLibJXR.cmake"
 
 if ($IsLinux) {
@@ -55,8 +61,12 @@ if ($IsMacOS) {
         $null
 }
 
+$argJxrEnabled =
+    $kfTagVersion -and $kfTagVersion -lt [version]'6.26.0' ? '-DKIMAGEFORMATS_JXR=ON' :
+    '-DKIMAGEFORMATS_WITH_KNOWN_CRASHES_JXR=ON'
+
 # Build kimageformats
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/installed" -DKIMAGEFORMATS_JXL=ON -DKIMAGEFORMATS_HEIF=ON -DKIMAGEFORMATS_JXR=ON -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" -DVCPKG_INSTALLED_DIR="$env:VCPKG_ROOT/installed-$env:VCPKG_DEFAULT_TRIPLET" $argDeviceArchs .
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/installed" -DKIMAGEFORMATS_JXL=ON -DKIMAGEFORMATS_HEIF=ON $argJxrEnabled -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" -DVCPKG_INSTALLED_DIR="$env:VCPKG_ROOT/installed-$env:VCPKG_DEFAULT_TRIPLET" $argDeviceArchs .
 
 ninja
 ninja install
@@ -76,7 +86,7 @@ if ($IsMacOS -and $env:buildArch -eq 'Universal') {
 
     [Environment]::SetEnvironmentVariable("KF${kfMajorVer}Archive_DIR", [Environment]::GetEnvironmentVariable("KF${kfMajorVer}Archive_DIR_INTEL"))
 
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/installed_intel" -DKIMAGEFORMATS_JXL=ON -DKIMAGEFORMATS_HEIF=ON -DKIMAGEFORMATS_JXR=ON -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" -DVCPKG_INSTALLED_DIR="$env:VCPKG_ROOT/installed-x64-osx" -DVCPKG_TARGET_TRIPLET="x64-osx" -DCMAKE_OSX_ARCHITECTURES="x86_64" .
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/installed_intel" -DKIMAGEFORMATS_JXL=ON -DKIMAGEFORMATS_HEIF=ON $argJxrEnabled -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" -DVCPKG_INSTALLED_DIR="$env:VCPKG_ROOT/installed-x64-osx" -DVCPKG_TARGET_TRIPLET="x64-osx" -DCMAKE_OSX_ARCHITECTURES="x86_64" .
 
     ninja
     ninja install

pwsh/get-vcpkg-deps.ps1
@@ -135,6 +135,9 @@ function WriteManifest() {
     AddDependency 'libheif' $null true
     AddDependency 'jxrlib'
 
+    # libde265 1.0.18 breaks HEIF decoder
+    AddOverride 'libde265' '1.0.16#1'
+
     $manifest | ConvertTo-Json -Depth 5 | Out-File -FilePath "$vcpkgManifestDir/vcpkg.json"
 }
 

util/kimageformats6-find-jxrlib-vcpkg-b.patch
@@ -1,0 +1,14 @@
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -101,7 +101,10 @@
+ # You should not enable it unless you know what you are doing.
+ option(KIMAGEFORMATS_WITH_KNOWN_CRASHES_JXR "Enable plugin for JPEG XR format" OFF)
+ if(KIMAGEFORMATS_WITH_KNOWN_CRASHES_JXR)
+-    find_package(LibJXR)
++    find_package(JXR)
++    set(LibJXR_FOUND ${JXR_FOUND})
++    set(LIBJXR_INCLUDE_DIRS ${JXR_INCLUDE_DIRS})
++    set(LIBJXR_LIBRARIES ${JXR_LIBRARIES})
+ endif()
+ add_feature_info(LibJXR LibJXR_FOUND "required for the QImage plugin for JPEG XR images")
+ 